b32723492bd7e4b1266fb37c669ae07544b0041e
Nimra <56455442+nimra200@users.noreply.github.com>
2021-07-01 13:42:56 -0400

    Add registration endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, Response, render_template, send_from_directory
 from dotenv import load_dotenv
 from . import db
+from werkzeug.security import check_password_hash, generate_password_hash
+from app.db import get_db 
 
 load_dotenv()
 app = Flask(__name__)
@@ -15,3 +17,32 @@ def index():
 @app.route('/health', methods=['GET'])
 def health():
     return Response("HTTP 200 OK", mimetype='text/html'), 200
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM user WHERE username = ?', (username,)
+        ).fetchone() is not None:
+            error = f"User {username} is already registered."
+
+        if error is None:
+            db.execute(
+                'INSERT INTO user (username, password) VALUES (?, ?)',
+                (username, generate_password_hash(password))
+            )
+            db.commit()
+            return f"User {username} created successfully"
+        else:
+            return error, 418
+
+    ## TODO: Return a register page
+    return "Register Page not yet implemented", 501

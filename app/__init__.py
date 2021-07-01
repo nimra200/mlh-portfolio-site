8b0d87ec5e5110c6b7dd458421dfdb7a352603e8
Cloud User <centos@ip-172-31-15-25.us-east-2.compute.internal>
2021-07-01 16:16:18 +0000

    Add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 import os
-from flask import Flask, Response, render_template, send_from_directory
+from flask import Flask, request, Response, render_template, send_from_directory
 from dotenv import load_dotenv
+from . import db
 
 load_dotenv()
 app = Flask(__name__)
-
+app.config['DATABASE'] = os.path.join(os.getcwd(), 'flask.sqlite')
+db.init_app(app)
 
 @app.route('/')
 def index():
